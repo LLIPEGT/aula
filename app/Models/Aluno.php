888235eb8c5100141ef194acc12724f5bdbaa109
Fellipe Fernandes Nogueira <fellipe.5fernades@mail.com>
2024-03-20 13:07:41 -0300

    Feito

---
+++ app/Models/Aluno.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Aluno extends Model
+{
+    use HasFactory;
+
+    public function comprovante() {
+        return $this->hasMany('\App\Models\Comprovante');
+    }
+
+    public function declaracao() {
+        return $this->hasMany('\App\Models\Declaracao');
+    }
+
+    public function user() {
+        return $this->belongsTo('\App\Models\User');
+    }
+
+    public function curso() {
+        return $this->belongsTo('\App\Models\Curso');
+    }
+
+    public function turma() {
+        return $this->belongsTo('\App\Models\Turma');
+    }
+}
